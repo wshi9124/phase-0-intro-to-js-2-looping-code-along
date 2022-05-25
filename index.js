27e85ee20c8955bf99e70f652a8b9fbe4476fe00
Willie Shi <wshi9124@gmail.com>
2022-05-25 15:19:11 -0400

    commit tree 19f18ce5deaed3afed706b2e6f0013aa50247ac9

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Charlie","Samip", "Ali"]
+const events = ["birthday"]
+
+function writeCards (names,events) {
+    let message=[]
+  for (let i=0; i < names.length; i++) {
+     message.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+    }
+
+  return message
+}
+
+const integer = []
+function countDown(integer) {
+  let i=10
+  while (i>=0) {
+    console.log (i--)
+  }
+  return integer
+}
